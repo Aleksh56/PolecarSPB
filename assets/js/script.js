938fbc6888808f58556b6a6728663a72088f02d7
AlexandrA <averyanovv.a@yandex.ru>
2022-08-10 13:33:16 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    // Hero slider
+
+    $('.hero__slider').slick({
+      arrows: true,
+      autoplay: true,
+      adaptiveHeight: false
+    });
+
+    function deleteTextInSlickSliderButtons(){
+      $('.slick-prev').html('<i class="fa-solid fa-angle-left"></i>')
+      $('.slick-next').html('<i class="fa-solid fa-angle-right"></i>')
+    }
+
+    // Burger-menu
+
+    $('.header__menu-burger').click(() => {
+      $('.header__menu-slide').addClass("header__slide-active", 2000)
+    });
+
+    $('.slide-xmark').click(() => {
+      $('.header__menu-slide').removeClass("header__slide-active", 2000)
+    });
+
+    deleteTextInSlickSliderButtons()
+  });
